92bd356ff6a8c5b318994a3e0481632660bd2be4
Jo Liss <joliss42@gmail.com>
2015-10-15 19:49:48 +0000

    Initial version

---
+++ README.md
@@ -0,0 +1,102 @@
+# node-multidep
+
+Install multiple versions of a package via npm.
+
+This is useful for integration testing. It's perhaps not robust enough for
+production code.
+
+## Installation
+
+```bash
+npm install --save-dev multidep
+```
+
+## Usage
+
+### Specifying which versions to install
+
+Create a JSON spec of packages to be installed, e.g. at `test/multidep.json`:
+
+```json
+{
+  "path": "test/multidep",
+  "versions": {
+    "broccoli": ["0.16.3", "1.0.0"]
+  }
+}
+```
+
+Do not use fuzzy versions (`"^0.16.0"`) - this will cause problems.
+
+### Installing the packages from npm
+
+Next, run `multidep test/multidep.json`. In this example, it will create
+`test/multidep` and install broccoli 0.16.3 and broccoli 1.0.0 somewhere
+inside the `test/multidep` directory.
+
+To run `multidep` automatically before `npm test`, add it as a "pretest"
+command to your `package.json`:
+
+```json
+{
+  "scripts": {
+    "pretest": "multidep test/multidep.json",
+    "test": "..."
+  }
+}
+```
+
+`multidep` will not redownload existing packages. If something went wrong,
+delete its directory first: `rm -r test/multidep`
+
+### Accessing specific package versions
+
+In your integration test, access the different versions like so:
+
+```js
+var multidepPackages = require('multidep')('test/multidep.json')
+
+var broccoli_0_16_3 = multidepPackages['broccoli']['0.16.3']()
+var broccoli_1_0_0 = multidepPackages['broccoli']['1.0.0']()
+```
+
+Note the trailing `()` - this is needed to `require` the module.
+
+### Iterating over all versions of a package
+
+To iterate over each version, use the `.forEachVersion` helper method.
+
+```js
+multidepPackages['broccoli'].forEachVersion(function(version, broccoli) {
+  // Do stuff with the `broccoli` module
+})
+```
+
+### Testing against the master branch
+
+Sometimes it's useful to test against your local checkout of a package. To do
+so, symlink it to `<path>/<package-name>-master`, e.g.
+
+```bash
+ln -s ~/src/broccoli test/multidep/broccoli-master
+```
+
+You do not need to list this "master" version in your `multidep.json`.
+`multidep` will detect its presence at runtime.
+
+```js
+var multidepPackages = require('multidep')('test/multidep.json')
+
+var broccoli_master = multidepPackages['broccoli']['master']()
+if (broccoli_master !== null) {
+  ...
+}
+
+// Or:
+multidepPackages['broccoli'].forEachVersion(function(version, broccoli) {
+  // If broccoli-master is present, this loop will have an extra iteration
+  // at the end with version === 'master'
+})
+```
+
+This only works for "master", not for arbitrary versions.
---
+++ index.js
@@ -0,0 +1,109 @@
+'use strict'
+
+var fs = require('fs')
+var path = require('path')
+var childProcess = require('child_process')
+var RSVP = require('rsvp')
+var rimraf = require('rimraf')
+
+// Check that all referenced versions referenced by the spec exist, and
+// additionally pick up master versions. Return a hash of packages.
+module.exports = function multidep(specPath) {
+  var spec = getSpec(specPath)
+
+  var packages = {}
+  Object.keys(spec.versions).sort().forEach(function(packageName) {
+    packages[packageName] = new PackageCollection
+    spec.versions[packageName].forEach(function(version) {
+      var packagePath = path.join(spec.path, packageName + '-' + version)
+      if (!fs.existsSync(packagePath)) {
+        throw new Error(packagePath + ': No such file or directory. Run `multidep` to install.')
+      }
+      var absPath = fs.realpathSync(path.join(packagePath, 'node_modules', packageName))
+      packages[packageName][version] = require.bind(global, absPath)
+      packages[packageName].versions.push(version)
+    })
+
+    var masterPath = path.join(spec.path, packageName + '-master')
+    if (fs.existsSync(masterPath)) {
+      var absPath = fs.realpathSync(masterPath)
+      packages[packageName]['master'] = require.bind(global, absPath)
+      packages[packageName].versions.push('master')
+    } else {
+      packages[packageName]['master'] = function() { return null }
+    }
+  })
+  return packages
+}
+
+module.exports.install = function(specPath) {
+  var spec = getSpec(specPath)
+
+  if (!fs.existsSync(spec.path)) {
+    fs.mkdirSync(spec.path)
+  }
+
+  var promise = RSVP.resolve()
+  Object.keys(spec.versions).sort().forEach(function(packageName) {
+    spec.versions[packageName].forEach(function(version) {
+      promise = promise.then(function() {
+        var packagePath = path.join(spec.path, packageName + '-' + version)
+        return RSVP.resolve()
+          .then(function() {
+            if (!fs.existsSync(packagePath)) {
+              console.log(packageName + ' ' + version + ': Installing')
+              fs.mkdirSync(packagePath)
+              fs.mkdirSync(path.join(packagePath, 'node_modules'))
+              var cp = childProcess.spawn('npm', ['install', packageName + '@' + version], {
+                cwd: packagePath,
+                stdio: 'inherit',
+                timeout: 300
+              })
+              return new RSVP.Promise(function(resolve, reject) {
+                cp.on('exit', function(code, signal) {
+                  if (code !== 0 || signal != null) {
+                    reject(new Error('npm exited with exit code ' + code + ', signal ' + signal))
+                  } else {
+                    resolve()
+                  }
+                })
+              })
+            } else {
+              console.log(packageName + ' ' + version + ': Installed')
+            }
+          })
+          .catch(function(err) {
+            // We created a nested promise with `RSVP.resolve()` above so this
+            // .catch clause only applies to the previous .then and doesn't
+            // catch earlier failures in the chain
+            rimraf.sync(packagePath)
+            throw err
+          })
+      })
+    })
+  })
+  return promise
+}
+
+function getSpec(specPath) {
+  // specPath is relative to cwd, so we need to call realpathSync
+  var spec = require(fs.realpathSync(specPath))
+  if (!spec || !spec.hasOwnProperty('path') || Array.isArray(spec.versions)) {
+    throw new Error('Invalid version spec; expected { path: "test/multidep", versions: { ... } }, got ' +
+      require('util').inspect(spec))
+  }
+  return spec
+}
+
+function PackageCollection() {
+  // We cannot use Object.keys(this) to get the versions because we want to
+  // preserve the order in which they are listed in the spec
+  this.versions = []
+}
+
+PackageCollection.prototype.forEachVersion = function(cb) {
+  for (var i = 0; i < this.versions.length; i++) {
+    var module = this[this.versions[i]]() // require
+    cb(this.versions[i], module)
+  }
+}
